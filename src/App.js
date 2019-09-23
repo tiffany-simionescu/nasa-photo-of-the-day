c370606acce2569356d6b0c7ac12ec616dfafa62
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-09-23 18:59:40 -0400

    Initial Commit - created axios API request, Step 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+  const [nasaImage, setNasaImage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(res => {
+        console.log(res);
+        setNasaImage(res.data.hdurl)})
+      .catch(err => console.log(err));
+  }, [])
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <img src={nasaImage} alt="Nasa Image of the Day" />
     </div>
   );
 }
