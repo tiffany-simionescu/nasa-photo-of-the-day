18401c9fcab5f63e820e7a4149e3b894abe369a7
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-09-23 19:04:05 -0400

    Completed steps 4-5, Need to work on steps 1-2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import "./App.css";
 
 function App() {
+  // Step 3 - Fetching the Data 
   const [nasaImage, setNasaImage] = useState("");
 
   useEffect(() => {
@@ -10,6 +11,7 @@ function App() {
       .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
       .then(res => {
         console.log(res);
+        // Step 4 - Adding the Data to State
         setNasaImage(res.data.hdurl)})
       .catch(err => console.log(err));
   }, [])
@@ -20,6 +22,7 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      {/* Step 5 - Display the Data */}
       <img src={nasaImage} alt="Nasa Image of the Day" />
     </div>
   );
