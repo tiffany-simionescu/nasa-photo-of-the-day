819170822be32d46209244a813c5b74b85f12449
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-09-24 18:12:08 -0400

    Updated useEffect

--- src/components/NasaImage.js
+++ src/components/NasaImage.js
@@ -1,24 +1,34 @@
 import React, {useState, useEffect} from 'react';
 import NasaImageContainer from "./NasaImageContainer.js";
-import Axios from 'axios';
+import axios from 'axios';
 
 export default function NasaImage() {
   const [nasaImage, setNasaImage] = useState("");
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const imageOfTheDay = await Axios.get(
-          "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
-        );
-        setNasaImage(imageOfTheDay.data.hdurl);
-        console.log(imageOfTheDay.data.hdurl);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const imageOfTheDay = await Axios.get(
+  //         "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+  //       );
+  //       setNasaImage(imageOfTheDay.data.hdurl);
+  //       console.log(imageOfTheDay.data.hdurl);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
+
+    useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(res => {
+        console.log(res.data.hdurl);
+        // Step 4 - Adding the Data to State
+        setNasaImage(res.data.hdurl)})
+      .catch(err => console.log(err));
+  }, [])
 
   return (
     <div className="image">
