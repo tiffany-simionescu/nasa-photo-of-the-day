b91f33bdd754436edaeb9436866a7b74296b6f49
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-09-24 21:26:14 -0400

    Fixed NasaImageContainer to display image

--- src/components/NasaImageContainer.js
+++ src/components/NasaImageContainer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NasaImageContainer(props) {
   return(
     <div className="image-container">
-      <img className="image" src={props.nasaImage} alt={props.alt} />
+      <img className="image" src={props.src} alt={props.alt} />
     </div>
   );
 }
